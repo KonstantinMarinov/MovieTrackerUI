c429c0d9d58e3606f3fe28bea96fd6f8157c0fb1
Konstantin Marinov <konstantin.marinov17@gmail.com>
2022-05-29 23:26:08 +0300

    Page not found component

--- MovieTrackerUI/src/app/pages/home/home-page.component.ts
+++ MovieTrackerUI/src/app/pages/home/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  name = "test";
+
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
+
+    
   }
 
 }
