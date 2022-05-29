1a4340ed8347f7a4cafc06459d3a6a8cf1d1490d
Konstantin Marinov <konstantin.marinov17@gmail.com>
2022-05-29 22:48:23 +0300

    File strcuture refactoring

--- MovieTrackerUI/src/app/app.module.ts
+++ MovieTrackerUI/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './pages/home-page.component';
-import { AboutPageComponent } from './pages/about-page.component';
+import { HomePageComponent } from './pages//home/home-page.component';
+import { AboutPageComponent } from './pages/about/about-page.component';
 
 
 @NgModule({
