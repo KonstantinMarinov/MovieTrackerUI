7aba8b364d3a5f441b806df36e4bacbd96432294
Konstantin Marinov <konstantin.marinov17@gmail.com>
2022-05-29 22:44:01 +0300

    Added navbar and home page with some styling

--- MovieTrackerUI/src/app/app.module.ts
+++ MovieTrackerUI/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CatalogPageComponent } from './catalog-page/catalog-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavigationComponent,
+    CatalogPageComponent,
+   
   ],
   imports: [
     BrowserModule,
